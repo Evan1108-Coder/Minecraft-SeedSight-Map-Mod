e7d2205362d5af0937484c9c63d5d01182aabfef
Megan Foster <meganfoster@visionclaw.dev>
2026-04-27 21:50:33 +0800

    chore: Configure for MC 1.21.0 (Tricky Trials)

--- src/main/java/com/worldwhisperer/ModVersion.java
+++ src/main/java/com/worldwhisperer/ModVersion.java
@@ -1,8 +1,8 @@
 package com.worldwhisperer;
 
 public final class ModVersion {
-    public static final String MC_VERSION = "1.21.4";
-    public static final int MC_MINOR = 4;
+    public static final String MC_VERSION = "1.21";
+    public static final int MC_MINOR = 0;
 
     public static boolean hasPaleGarden() {
         return MC_MINOR >= 4;
